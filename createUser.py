f938e4ff83b4c0d3d027010379fa6f28cd472c26
Jonathan Salinas <a.jonathan.sl@gmail.com>
2015-05-19 13:07:11 -0500

    Crear usuarios

---
+++ createUser.py
@@ -0,0 +1,25 @@
+import requests, json
+
+#url = 'http://localhost:8888/webservices/usuarios/create/'
+
+url = 'http://alcoholibrate.mx/webservices/usuarios/create'
+
+
+params = {
+	'nombre':'Jonathan',
+	'apellidos':'Salinas',
+	'correo': 'jhonzya@hotmail.com',
+	'password': 'jhonzya123',
+	'telefono': '12312345',
+	'ubicacion': 'EDOMX',
+	'idTarjeta': '1234567890',
+	'telefono2': '12312345',
+	}
+
+
+# Headers necesarios
+headers = {'Content-Type': 'application/json'}
+
+r = requests.get(url, data=json.dumps(params), headers=headers)
+
+print r.text
